00b7498e96b7267f82cf9406dfc52c1109530fb9
Dominic K Savio <dominicksavio>
2021-06-05 19:53:11 +0530

    Django static configured to all

--- settings.py
+++ settings.py
@@ -129,6 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR
 LOGIN_REDIRECT_URL = '/accounts/dashboard/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR/'media'
